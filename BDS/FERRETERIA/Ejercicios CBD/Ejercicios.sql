94eb88442791d2fcb3473d45f17d61e5b6a342e0
CarlosDRomero <127170508+CarlosDRomero@users.noreply.github.com>
2023-09-05 11:50:48 -0500

    Update Ejercicios.sql

--- BDS/FERRETERIA/Ejercicios CBD/Ejercicios.sql
+++ BDS/FERRETERIA/Ejercicios CBD/Ejercicios.sql
@@ -1,8 +1,30 @@
 set serveroutput on
+DECLARE
+  CURSOR cursor_iva IS SELECT NOMBRE, PRECIO_VENTA, IVA FROM PRODUCTO ORDER BY PRECIO_VENTA;
+  
+  v_nombre PRODUCTO.NOMBRE%TYPE;
+  v_precio_venta PRODUCTO.PRECIO_VENTA%TYPE;
+  v_IVA PRODUCTO.IVA%TYPE;
+  precio_iva NUMBER;
+  
+BEGIN
+  OPEN cursor_iva;
+  LOOP
+    FETCH cursor_iva INTO v_nombre, v_precio_venta, v_iva;
+    PRECIO_IVA := v_precio_venta*(1+(v_iva/100));
+    DBMS_OUTPUT.PUT_LINE('NOMBRE: ' || v_nombre || ' PRECIO BASE: ' || v_precio_venta || ' PRECIO CON IVA: ' || PRECIO_IVA);
+    EXIT WHEN cursor_iva%NOTFOUND;
+  END LOOP;
+  CLOSE cursor_iva;
+END;
+/
+
+set serveroutput on
 
 -- CONTAR NUMEROS DEL 1 AL 10
 CREATE OR REPLACE FUNCTION funcion_contar RETURN NUMBER IS
 BEGIN
+    dbms_output.put_line('FUNCION MOSTRAR NUMEROS DEL 1 AL 10');
     FOR i in 1..10 loop
         dbms_output.put_line(i);
     end loop;
@@ -12,6 +34,7 @@ END funcion_contar;
 
 CREATE OR REPLACE PROCEDURE procedimiento_contar AS
 BEGIN
+    dbms_output.put_line('PROCEDIMIENTO MOSTRAR NUMEROS DEL 1 AL 10');
     FOR i in 1..10 loop
         dbms_output.put_line(i);
     end loop;
